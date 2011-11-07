035138fee52c4d03a16e4b5a7406d71350b57510
FG Ribreau <github@fgribreau.com>
2011-11-07 17:44:15 +0100

    Ajout d'un initialiser pour Check

--- app/geobbs_templated/geobbs/Models/Check.h
+++ app/geobbs_templated/geobbs/Models/Check.h
@@ -20,4 +20,8 @@
 @property(retain) NSDate *date;
 @property(retain) NSString *description;
 
+
+
+- (id)initWithLocation: (CLLocation *)location userId:(NSString *)userId description:(NSString *)description;
+
 @end
--- app/geobbs_templated/geobbs/Models/Check.m
+++ app/geobbs_templated/geobbs/Models/Check.m
@@ -16,6 +16,17 @@
 @synthesize date;
 @synthesize description;
 
+- (id)initWithLocation: (CLLocation*)_location userId:(NSString*)_userId description:(NSString*)_description{
+	
+    if (self = [super init])
+    {
+        [self setLocation:_location];
+        [self setUserId:_userId];
+        [self setDescription:_description];
+    }
+    return self;
+}
+
 
 - (void)dealloc {
     [super dealloc];
